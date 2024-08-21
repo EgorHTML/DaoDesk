8b09a5df252f5fd25027fc6c7c8ee1397aa081bb
Egor <jdmjapanese69@gmail.com>
2024-08-21 15:50:15 +0300

    header, primary button

--- src/blocks/HeaderBlock.vue
+++ src/blocks/HeaderBlock.vue
@@ -1,3 +1,92 @@
+<script setup>
+import DaoDeskLogo from '../components/logo/DaoDeskLogo.vue';
+import PrimaryButton from '../components/buttons/PrimaryButton.vue';
+</script>
+
 <template>
-    header
+    <header class="main_header">
+        <div class="logo">
+            <DaoDeskLogo />
+        </div>
+        <nav>
+            <ul class="main_header__navigation">
+                <li>
+                    <span>About us</span>
+                </li>
+                <li>
+                    <span>Pricing</span>
+                </li>
+                <li>
+                    <span>Demo</span>
+                </li>
+            </ul>
+        </nav>
+        <div>
+            <ul class="main_header__buttons">
+                <li>
+                    <PrimaryButton text="free trial" />
+                </li>
+                <li>
+                    <PrimaryButton text="Buy now" />
+                </li>
+            </ul>
+        </div>
+    </header>
 </template>
+
+<style>
+.main_header__navigation,
+.main_header__buttons,
+.main_header {
+    display: flex;
+}
+
+.main_header {
+    align-items: center;
+    font-size: 16px;
+    justify-content: center;
+    height: 100px;
+    border-bottom: 1px solid rgba(202, 194, 178, 0.85);
+    position: sticky;
+}
+
+.main_header nav {
+    height: 100%;
+}
+
+.main_header__navigation {
+    height: 100%;
+    gap: 30px;
+    margin-left: 50px;
+    margin-right: 50px;
+    align-items: center;
+}
+
+.main_header__navigation li {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    position: relative;
+}
+
+.main_header__navigation li span::after {
+    content: "";
+    height: 3px;
+    background: #000;
+    bottom: 0px;
+    position: absolute;
+    left: 0px;
+    width: 0;
+    transition: all 0.25s ease;
+}
+
+.main_header__navigation li:hover span::after {
+    width: 100%;
+}
+
+.logo,
+.main_header__navigation,
+.main_header__buttons button {
+    cursor: pointer;
+}
+</style>
---
+++ src/components/buttons/PrimaryButton.vue
@@ -0,0 +1,61 @@
+<script setup>
+defineProps({
+    text: String
+})
+</script>
+
+<template>
+    <button class="primary_button">
+        <div style="overflow: hidden;">
+            <span>
+                {{ text }}
+            </span>
+        </div>
+    </button>
+</template>
+
+<style>
+@keyframes ButtonTextSwitch {
+    0% {
+        transform: translateY(0);
+    }
+
+    30% {
+        transform: translateY(-10px);
+    }
+
+    31% {
+        transform: translateY(15px);
+    }
+
+    100% {
+        transform: translateY(0);
+    }
+}
+
+.primary_button {
+    color: #fff;
+    outline: 0;
+    border: 1px solid rgb(154, 68, 151);
+    border-radius: 25px;
+    width: 100px;
+    height: 40px;
+    background-color: rgb(154, 68, 151);
+
+    transition: background-color 500ms cubic-bezier(0.85, 0, 0.15, 1), border 500ms cubic-bezier(0.85, 0, 0.15, 1), transform 500ms cubic-bezier(0.85, 0, 0.15, 1), height 500ms cubic-bezier(0.85, 0, 0.15, 1), opacity 500ms cubic-bezier(0.85, 0, 0.15, 1), width 500ms cubic-bezier(0.85, 0, 0.15, 1), outline 70ms cubic-bezier(0, 0, 0.5, 1);
+}
+
+.primary_button:hover {
+    background-color: black;
+    border-color: black;
+}
+
+.primary_button span {
+    display: block;
+    transition: color 500ms cubic-bezier(0.85, 0, 0.15, 1), transform 500ms cubic-bezier(0.85, 0, 0.15, 1);
+}
+
+.primary_button:hover span {
+    animation: ButtonTextSwitch ease 0.6s forwards;
+}
+</style>

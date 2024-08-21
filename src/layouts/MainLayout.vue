108bd02de61e8ed337b377ad8f2730c72eba51dc
Egor <jdmjapanese69@gmail.com>
2024-08-21 12:51:03 +0300

    router added

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,11 +1,10 @@
 <script setup>
 import HeaderBlock from '../blocks/HeaderBlock.vue';
 import FooterBlock from '../blocks/FooterBlock.vue';
-import MainView from '../views/MainView.vue';
 </script>
 
 <template>
     <HeaderBlock />
-    <MainView />
+    <RouterView />
     <FooterBlock />
 </template>

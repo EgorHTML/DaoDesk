fe51a542cd1fc56a3bc22e470013c71f52044098
Egor <jdmjapanese69@gmail.com>
2024-08-21 12:38:34 +0300

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HeaderBlock from '../blocks/HeaderBlock.vue';
+import FooterBlock from '../blocks/FooterBlock.vue';
+import MainView from '../views/MainView.vue';
+</script>
+
+<template>
+    <HeaderBlock />
+    <MainView />
+    <FooterBlock />
+</template>
